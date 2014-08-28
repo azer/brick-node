a27cd18975a29fce7526ac4ffdd2d83858534ca5
E.Azer Koçulu <azer@kodfabrik.com>
2014-08-28 00:07:14 -0700

    move cli

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var Struct = require("new-struct");
+var cli = require('./lib/cli');
 var methods = require('./lib/methods');
 
 module.exports = Struct(methods);
@@ -6,3 +7,4 @@ module.exports = Struct(methods);
 module.exports.create = require("./lib/create-class");
 module.exports.setup = require("./lib/setup");
 module.exports.generateMap = require("./lib/generate-map");
+module.exports.cli = cli;
